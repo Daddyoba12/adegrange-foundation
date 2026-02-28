46c05950a2d51ddf9e031c83d7db302294628c6c
Babs Olufeko <daddyoba12@gmail.com>
2026-02-28 02:13:55 +0000

    Fix FinancialChart export for production build

--- components/FinancialChart.tsx
+++ components/FinancialChart.tsx
@@ -1,7 +1,7 @@
-<div className="mt-32">
-  <h2 className="text-3xl font-semibold text-center mb-12">
-    Program Growth
-  </h2>
-
-  <FinancialChart />
-</div>
+export default function FinancialChart() {
+  return (
+    <div>
+      {/* your chart code */}
+    </div>
+  )
+}
